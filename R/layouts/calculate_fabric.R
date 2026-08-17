320536002525eeacf7a908c9d7ee0a50d89e0569
libjan <libuse.janska@gmail.com>
2026-08-17 23:38:31 +0200

    added fabric recommendation calculation

---
+++ R/layouts/calculate_fabric.R
@@ -0,0 +1,25 @@
+#' Calculate required fabric length
+#'
+#' @param layout A layout object
+#' @param margin Desired margin for fabric recommendation
+#'
+#' @returns a fabric size object
+#'
+#' @export
+#' @examples
+calculate_fabric <- function(layout, margin = 10) {
+  if (is.null(layout)) {
+    stop("Needs a valid layout object")
+  }
+  if (is.null(layout$fabric$length)) {
+    stop("Fabric length in layout object is missing")
+  }
+  fabric_result <- list(
+    required_length = layout$fabric$length,
+    recommended_length = required_length + margin,
+    fabric_width = layout$fabric$width,
+    margin = margin,
+    units = "cm"
+  )
+  return(fabric_result)
+}
