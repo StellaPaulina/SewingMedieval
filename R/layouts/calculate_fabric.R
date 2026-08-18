a96323c09d82a2e47cbcab1ca74b14d39d32a8cd
libjan <libuse.janska@gmail.com>
2026-08-18 22:38:48 +0200

    fixed bug in list variable assignment

--- R/layouts/calculate_fabric.R
+++ R/layouts/calculate_fabric.R
@@ -14,8 +14,9 @@ calculate_fabric <- function(layout, margin = 10) {
   if (is.null(layout$fabric$length)) {
     stop("Fabric length in layout object is missing")
   }
+  required_length <- layout$fabric$length
   fabric_result <- list(
-    required_length = layout$fabric$length,
+    required_length = required_length,
     recommended_length = required_length + margin,
     fabric_width = layout$fabric$width,
     margin = margin,
