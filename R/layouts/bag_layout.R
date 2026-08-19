606120353930848a4024376762c58385f6c8625b
libjan <libuse.janska@gmail.com>
2026-08-19 11:54:52 +0200

    updated bag_layout function to fix rotation behavior

--- R/layouts/bag_layout.R
+++ R/layouts/bag_layout.R
@@ -14,39 +14,51 @@ bag_layout <- function(pattern, fabric_width){
     if (pattern$id != "bag") {
         stop("Invalid pattern type")
     }
-    if (is.null(pattern$pieces$full)){
+    if (is.null(pattern$pieces[[1]]$id)){
         stop("Bag pattern piece full is required")
     }
     # find bounds of pattern piece
     full_bbox <- st_bbox(pattern$pieces[[1]]$geometry)
     # find the x and y dimensions
-    dim_x  <- full_bbox["xmax"] - full_bbox["xmin"]
-    dim_y <- full_bbox["ymax"] - full_bbox["ymin"]
-    if (min(dim_x, dim_y) > fabric_width) { #there is no way to rotate
+    dim_x  <- full_bbox["xmax"][[1]] - full_bbox["xmin"][[1]]
+    dim_y <- full_bbox["ymax"][[1]] - full_bbox["ymin"][[1]]
+    
+    # if all orientations fit the fabric length
+    if (max(dim_x, dim_y) <= fabric_width) {
+        index_min <- which(c(dim_x, dim_y) == min(c(dim_x, dim_y)))
+        # plot with smaller dimension as the length of fabric
+        if (index_min == 1) {
+            angle <- 0
+            x <- 0
+            y <- 0
+            minimum_fabric <- dim_x
+        }
+        else {
+            angle <- 90
+            x <- dim_y
+            y <- 0
+            minimum_fabric <- dim_y
+        }
+    }
+    else if (min(dim_x, dim_y) > fabric_width) { #there is no way to rotate
         stop("Pattern is too large for fabric width")
     }
-    if (dim_x > dim_y & dim_y <= fabric_width) {
-        # rectangle must be rotated to fit fabric length
-        angle <- 90
-        # rectangle must be moved to fit properly to origin after rotation
-        x <- dim_y[[1]]
-        minimum_fabric <- dim_y[[1]]
-    }
-    # optionally could add in a rotation to further minimize length for dim_x > dim_y
-    #else if (dim_x > dim_y ) {
-    #    # trigonometry to determin rectangle rotation
-    #    d = sqrt(dim_x^2 + dim_y^2)
-    #    triangle_angle <- asin(fabric_width/d) * 180 / pi
-    #    h_angle <- asin(dim_y/d) * 180 / pi
-    #    # set angle
-    #    angle <- triangle_angle - h_angle
-    #    #check that angle does produce shorter fabric length than no rotation
-    #}
     else {
-        angle <- 0
-        x = 0
-        minimum_fabric <- dim_x[[1]]
+        index_min <- which(c(dim_x, dim_y) == min(c(dim_x, dim_y)))
+        if (index_min == 2) {
+            angle <- 0
+            x <- 0
+            y <- 0
+            minimum_fabric <- dim_x
+        }
+        else {
+            angle <- 90
+            x <- dim_y
+            y <- 0
+            minimum_fabric <- dim_y
+        }
     }
+    
     layout_result <- list(
         fabric = list(
             width = fabric_width,
@@ -58,7 +70,7 @@ bag_layout <- function(pattern, fabric_width){
             list(
                 piece_id = "full",
                 x = x,
-                y = 0,
+                y = y,
                 rotation = angle,
                 flipped = FALSE
             )
