e8a8ba1507d5591c4cc7f662f45294c02d129051
libjan <libuse.janska@gmail.com>
2026-08-19 12:27:40 +0200

    added circleskirt pattern layout function

--- R/layouts/circleskirt_pattern_layout.R
+++ R/layouts/circleskirt_pattern_layout.R
@@ -1,3 +1,29 @@
-circleskirt_pattern_layout <- function() {
-
+#' Generate circleskirt pattern layout
+#'
+#' @param pattern circleskirt pattern object
+#'
+#' @returns list of placements of circleskirt pattern pieces
+#'
+#' @export
+#' @examples
+circleskirt_pattern_layout <- function(pattern) {
+  layout_result <- list(
+    placements = list(
+      list(
+        piece_id = "side1",
+        x = 0,
+        y = 0,
+        rotation = 0,
+        flipped = FALSE
+      ),
+      list(
+        piece_id = "side2",
+        x = 0,
+        y = 0,
+        rotation = 180,
+        flipped = FALSE
+      )
+    )
+  )
+  return(layout_result)
 }
